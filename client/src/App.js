5c7f48ecabf3b1215732388776bd8d244037672d
ThibaMahlezana <thiba.ma@gmail.com>
2022-06-21 02:45:54 +0200

    Website structure setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      Tiye
+      <Navbar />
+      <Home />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
