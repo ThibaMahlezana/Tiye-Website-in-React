7828c7cc454a5066cf0f3c1d5481032fe3bea4a1
ThibaMahlezana <thiba.ma@gmail.com>
2022-07-24 01:45:01 +0200

    Added about component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,9 +23,9 @@ export default function Header() {
         <Navbar.Collapse className="justify-content-end">
           <Nav className="me-auto">
             <Nav.Link className="nav-item" href="#home">Home</Nav.Link>
-            <Nav.Link className="nav-item" href="#link">About</Nav.Link>
-            <Nav.Link className="nav-item" href="#link">Services</Nav.Link>
-            <Nav.Link className="nav-item" href="#link">Portfolio</Nav.Link>
+            <Nav.Link className="nav-item" href="#about">About</Nav.Link>
+            <Nav.Link className="nav-item" href="#services">Services</Nav.Link>
+            <Nav.Link className="nav-item" href="#portfolio">Portfolio</Nav.Link>
             <button className="btn float-end"><a href="#contact">Contact</a></button>
           </Nav>
         </Navbar.Collapse>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import '../styles/global.css';
+import '../styles/about.css';
+import Container from 'react-bootstrap/Container';
+import bgImage from '../images/home-bg.png';
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="about" id="about">
+      <Container>
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1>About us</h1>
+          </div>
+          <div className="col-lg-6">
+            <h3>HELLO, WE ARE TIYE TECHNOLOGIES</h3>
+            <i class="fas fa-smile"></i>
+            <p>
+              <span className="about-tags">Design</span>
+              <span className="about-tags">Develop</span>
+              <span className="about-tags">Host</span>
+            </p>
+            <p className="about-description">
+              We are a custom software development company, providing a complete
+              software design and development service. We help individuals or
+              companies to bring their software ideas to life.
+            </p>
+            <p className="about-quote">
+              <i
+                class="fa fa-quote-left fa-3x fa-pull-left fa-border"
+                aria-hidden="true"
+              ></i>
+              Ideas are the currency of the future. ― Kevin Roberts
+              <i
+                class="fa fa-quote-right fa-3x fa-pull-right fa-border"
+                aria-hidden="true"
+              ></i>
+            </p>
+          </div>
+          <div className="col-lg-6 d-none d-sm-block">
+            <div className="about-image"><img src={bgImage} /></div>
+          </div>
+        </div>
+      </Container>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ccd8e2"
+          fill-opacity="1"
+          d="M0,192L80,208C160,224,320,256,480,261.3C640,267,800,245,960,245.3C1120,245,1280,267,1360,277.3L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+        ></path>
+      </svg>
+    </div>
   )
 }
