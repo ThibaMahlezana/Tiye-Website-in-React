1300791cbb066e15b432439e97dcdfa2081a3bd8
ThibaMahlezana <thiba.ma@gmail.com>
2022-07-23 23:25:50 +0200

    added navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../styles/global.css';
+import '../styles/navbar.css';
+import logo from '../images/logo.png';
 
-export default function Navbar() {
+export default function Header() {
   return (
-    <div>Navbar</div>
+    <Navbar expand="md" className="nav">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="me-auto">
+            <Nav.Link className="nav-item" href="#home">Home</Nav.Link>
+            <Nav.Link className="nav-item" href="#link">About</Nav.Link>
+            <Nav.Link className="nav-item" href="#link">Services</Nav.Link>
+            <Nav.Link className="nav-item" href="#link">Portfolio</Nav.Link>
+            <button className="btn float-end"><a href="#contact">Contact</a></button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
